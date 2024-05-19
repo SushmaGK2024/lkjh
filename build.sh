0d42ef86354e26ea028560ce3aeca00cf2a1808c
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-19 17:02:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-# Update Rust toolchain
-rustup update stable
-
 # Set writable directories for cargo
-export CARGO_HOME=/home/render/.cargo
-export CARGO_TARGET_DIR=/home/render/.cargo_target
+export CARGO_HOME=$HOME/.cargo
+export CARGO_TARGET_DIR=$HOME/.cargo_target
 
-# Install system dependencies
-apt-get update && apt-get install -y build-essential libssl-dev pkg-config
-
-# Install Python dependencies
+# Install dependencies
+pip install numpy
+pip install tokenizers
 pip install -r requirements.txt
+
+
+
