be894710078c846f23da978451e4d7f8cc78483f
SushmaGK2024 <sushmagkulkarni23@gmail.com>
2024-05-19 16:36:35 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Set writable directories for cargo
+export CARGO_HOME=$HOME/.cargo
+export CARGO_TARGET_DIR=$HOME/.cargo_target
+
+# Install dependencies
+pip install numpy
+pip install tokenizers
